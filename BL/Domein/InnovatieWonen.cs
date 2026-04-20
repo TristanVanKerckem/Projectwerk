62ea89c14b3c0e128d6ce2d3a00c236ed798b6d9
Carlos Youssef <carlos.youssef@student.hogent.be>
2026-04-20 11:47:14 +0200

    carlos

--- BL/Domein/InnovatieWonen.cs
+++ BL/Domein/InnovatieWonen.cs
@@ -1,10 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using ProjectbeheerBL.Domein;
 
-namespace ProjectbeheerBL.Domein
+public class InnovatieWonen : Project
 {
-    public class InnovatieWonen : Project
+    public int AantalWooneenheden { get; set; }
+    public List<WoonvormType> WoonvormTypes { get; set; } = new List<WoonvormType>();
+    public bool HeeftRondleiding { get; set; }
+    public bool HeeftShowcase { get; set; }
+    public double ArchitectuurScore { get; set; }
+    public bool HeeftSamenwerkingErfgoedOfToerisme { get; set; }
+
+    public InnovatieWonen(string titel, DateTime startDatum, string beschrijving, ProjectFase fase, Locatie locatie, int aantalWooneenheden, bool heeftRondleiding, bool heeftShowcase, double architectuurScore, bool heeftSamenwerkingErfgoedOfToerisme)
+        : base(titel, startDatum, beschrijving, fase, locatie)
     {
+        AantalWooneenheden = aantalWooneenheden;
+        HeeftRondleiding = heeftRondleiding;
+        HeeftShowcase = heeftShowcase;
+        ArchitectuurScore = architectuurScore;
+        HeeftSamenwerkingErfgoedOfToerisme = heeftSamenwerkingErfgoedOfToerisme;
     }
+
+    public InnovatieWonen() { }
 }
