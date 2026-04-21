fa741a61e577bd75c7aef5a3162a934efb677a35
adzm1 <adel.yt999@gmail.com>
2026-04-21 16:35:31 +0200

    repo adel

--- DL/Repository/GroeneRuimteRepository.cs
+++ DL/Repository/GroeneRuimteRepository.cs
@@ -4,62 +4,62 @@ using System.Text;
 using ProjectbeheerBL.Domein;
 using Microsoft.Data.SqlClient;
 
-namespace ProjectbeheerDL.Repository {
-    public class GroeneRuimteRepository : ProjectRepository {
+//namespace ProjectbeheerDL.Repository {
+//    //public class GroeneRuimteRepository : ProjectRepository {
 
 
-        public GroeneRuimteRepository(string connectionstring) : base(connectionstring) {}
+//    //    public GroeneRuimteRepository(string connectionstring) : base(connectionstring) {}
 
-        public void VoegProjectToe(Project project) 
-        {
-            // Specifieke code voor groene ruimte projecten
-            base.VoegProjectToe(project);
-            GroeneRuimte g = (GroeneRuimte)project;
+//    //    public void VoegProjectToe(Project project) 
+//    //    {
+//    //        // Specifieke code voor groene ruimte projecten
+//    //        base.VoegProjectToe(project);
+//    //        GroeneRuimte g = (GroeneRuimte)project;
 
 
-            using(sqlConnection conn = new sqlConnection(connectionstring)) {
-                string sql = "INSERT INTO GroeneRuimte (id, oppervlakte, biodiversiteit, aantalWandelpaden, inToeristischeWandelroute, beoordeling) " +
-                              "VALUES (@id, @opp, @bio, @paden, @route, @beoordeling)";
+//    //        using(sqlConnection conn = new sqlConnection(connectionstring)) {
+//    //            string sql = "INSERT INTO GroeneRuimte (id, oppervlakte, biodiversiteit, aantalWandelpaden, inToeristischeWandelroute, beoordeling) " +
+//    //                          "VALUES (@id, @opp, @bio, @paden, @route, @beoordeling)";
 
-                SqlCommand cmd = new SqlCommand(sql, conn);
-                cmd.Parameters.AddWithValue("@id", g.Id);
-                cmd.Parameters.AddWithValue("@opp", g.Oppervlakte);
-                cmd.Parameters.AddWithValue("@bio", g.Biodiversiteit);
-                cmd.Parameters.AddWithValue("@paden", g.AantalWandelpaden);
-                cmd.Parameters.AddWithValue("@route", g.IsInToeristWandelroute);
-                cmd.Parameters.AddWithValue("@beoordeling", g.Beoordeling);
+//    //            SqlCommand cmd = new SqlCommand(sql, conn);
+//    //            cmd.Parameters.AddWithValue("@id", g.Id);
+//    //            cmd.Parameters.AddWithValue("@opp", g.Oppervlakte);
+//    //            cmd.Parameters.AddWithValue("@bio", g.Biodiversiteit);
+//    //            cmd.Parameters.AddWithValue("@paden", g.AantalWandelpaden);
+//    //            cmd.Parameters.AddWithValue("@route", g.IsInToeristWandelroute);
+//    //            cmd.Parameters.AddWithValue("@beoordeling", g.Beoordeling);
 
-                conn.Open();
-                cmd.ExecuteNonQuery();
-            }
-        }
+//    //            conn.Open();
+//    //            cmd.ExecuteNonQuery();
+//    //        }
+//    //    }
 
-        public List<GroeneRuimte> GeefGroeneRuimtes()
-        {
-            List<GroeneRuimte> lijst = new List<GroeneRuimte>();
-            using (SqlConnection conn = new SqlConnection(_connectionString))
-            {
-                string sql = @"SELECT p.*, g.* FROM GroeneRuimte g 
-                       JOIN Project p ON g.Id = p.Id";
+//    //    public List<GroeneRuimte> GeefGroeneRuimtes()
+//    //    {
+//    //        List<GroeneRuimte> lijst = new List<GroeneRuimte>();
+//    //        using (SqlConnection conn = new SqlConnection(_connectionString))
+//    //        {
+//    //            string sql = @"SELECT p.*, g.* FROM GroeneRuimte g 
+//    //                   JOIN Project p ON g.Id = p.Id";
 
-                SqlCommand cmd = new SqlCommand(sql, conn);
-                conn.Open();
-                using (SqlDataReader reader = cmd.ExecuteReader())
-                {
-                    while (reader.Read())
-                    {
-                        var g = new GroeneRuimte
-                        {
-                            Id = (int)reader["Id"],
-                            Titel = reader["Titel"].ToString(),
-                            Oppervlakte = (double)reader["Oppervlakte"],
-                            IsInToeristWandelroute = (bool)reader["IsInToeristWandelroute"]
-                        };
-                        lijst.Add(g);
-                    }
-                }
-            }
-            return lijst;
-        }
-    }
-}
+//    //            SqlCommand cmd = new SqlCommand(sql, conn);
+//    //            conn.Open();
+//    //            using (SqlDataReader reader = cmd.ExecuteReader())
+//    //            {
+//    //                while (reader.Read())
+//    //                {
+//    //                    var g = new GroeneRuimte
+//    //                    {
+//    //                        Id = (int)reader["Id"],
+//    //                        Titel = reader["Titel"].ToString(),
+//    //                        Oppervlakte = (double)reader["Oppervlakte"],
+//    //                        IsInToeristWandelroute = (bool)reader["IsInToeristWandelroute"]
+//    //                    };
+//    //                    lijst.Add(g);
+//    //                }
+//    //            }
+//    //        }
+//    //        return lijst;
+//    //    }
+//    //}
+//}
--- DL/Repository/StadsontwikkelingRepository.cs
+++ DL/Repository/StadsontwikkelingRepository.cs
@@ -4,74 +4,74 @@ using System.Text;
 using ProjectbeheerBL.Domein;
 using Microsoft.Data.SqlClient;
 
-namespace ProjectbeheerDL.Repository {
-    public class StadsontwikkelingRepository : ProjectRepository {
-        public StadsontwikkelingRepository(string connectionstring) : base(connectionstring) {}
+//namespace ProjectbeheerDL.Repository {
+//    public class StadsontwikkelingRepository : ProjectRepository {
+//        public StadsontwikkelingRepository(string connectionstring) : base(connectionstring) {}
 
-        public void VoegProjectToe(Project project) {
-            // Specifieke code voor stadsontwikkeling projecten
-            base.VoegProjectToe(project);
-            StadOntwikkeling s= (StadOntwikkeling)project;
+//        public void VoegProjectToe(Project project) {
+//            // Specifieke code voor stadsontwikkeling projecten
+//            base.VoegProjectToe(project);
+//            StadOntwikkeling s= (StadOntwikkeling)project;
 
 
-            using(sqlConnection conn = new sqlConnection(connectionstring)) {
-                string sql = "INSERT INTO StadsOntwikkeling (id, vergunningsStatus, architectueeleWaarde, toegankelijkheid, bezienswaardigheid, uitlegbord, infowandeling) " +
-                              "VALUES (@id, @verg, @arch, @toeg, @beziens, @bord, @info)";
+//            using(sqlConnection conn = new sqlConnection(connectionstring)) {
+//                string sql = "INSERT INTO StadsOntwikkeling (id, vergunningsStatus, architectueeleWaarde, toegankelijkheid, bezienswaardigheid, uitlegbord, infowandeling) " +
+//                              "VALUES (@id, @verg, @arch, @toeg, @beziens, @bord, @info)";
 
-                SqlCommand cmd = new SqlCommand(sql, conn);
-                cmd.Parameters.AddWithValue("@id", s.Id);
-                cmd.Parameters.AddWithValue("@verg", (int)s.VergunningStatus);
-                [cite_start]// Feedback Tommy: HeeftArchitecturaleWaarde [cite: 4]
-                cmd.Parameters.AddWithValue("@arch", s.HeeftArchitecturaleWaarde);
-                cmd.Parameters.AddWithValue("@toeg", (int)s.Toegankelijkheid);
-                cmd.Parameters.AddWithValue("@beziens", s.IsBezienswaardig);
-                cmd.Parameters.AddWithValue("@bord", s.HeeftInfo);
-                cmd.Parameters.AddWithValue("@info", s.HeeftArchitecturaleWaarde);
+//                SqlCommand cmd = new SqlCommand(sql, conn);
+//                cmd.Parameters.AddWithValue("@id", s.Id);
+//                cmd.Parameters.AddWithValue("@verg", (int)s.VergunningStatus);
+//                [cite_start]// Feedback Tommy: HeeftArchitecturaleWaarde [cite: 4]
+//                cmd.Parameters.AddWithValue("@arch", s.HeeftArchitecturaleWaarde);
+//                cmd.Parameters.AddWithValue("@toeg", (int)s.Toegankelijkheid);
+//                cmd.Parameters.AddWithValue("@beziens", s.IsBezienswaardig);
+//                cmd.Parameters.AddWithValue("@bord", s.HeeftInfo);
+//                cmd.Parameters.AddWithValue("@info", s.HeeftArchitecturaleWaarde);
 
-                conn.Open();
-                cmd.ExecuteNonQuery();
+//                conn.Open();
+//                cmd.ExecuteNonQuery();
 
-                [cite_start]// Koppeling met Bouwfirmas (Veel-op-veel tabel) 
-                foreach (var firma in s.Bouwfirmas)
-                {
-                    string sqlLink = "INSERT INTO Bouwfirma_Stadsontwikkeling (bouwfirmaId, stadsontwikkelingid) VALUES (@fId, @sId)";
-                    SqlCommand cmdLink = new SqlCommand(sqlLink, conn);
-                    cmdLink.Parameters.AddWithValue("@fId", firma.Id);
-                    cmdLink.Parameters.AddWithValue("@sId", s.Id);
-                    cmdLink.ExecuteNonQuery();
-                }
-            }
-        }
+//                [cite_start]// Koppeling met Bouwfirmas (Veel-op-veel tabel) 
+//                foreach (var firma in s.Bouwfirmas)
+//                {
+//                    string sqlLink = "INSERT INTO Bouwfirma_Stadsontwikkeling (bouwfirmaId, stadsontwikkelingid) VALUES (@fId, @sId)";
+//                    SqlCommand cmdLink = new SqlCommand(sqlLink, conn);
+//                    cmdLink.Parameters.AddWithValue("@fId", firma.Id);
+//                    cmdLink.Parameters.AddWithValue("@sId", s.Id);
+//                    cmdLink.ExecuteNonQuery();
+//                }
+//            }
+//        }
 
-        public List<Stadsontwikkeling> GeefStadsontwikkelingen()
-        {
-            List<Stadsontwikkeling> lijst = new List<Stadsontwikkeling>();
-            using (SqlConnection conn = new SqlConnection(_connectionString))
-            {
-                // SQL-query met JOIN om zowel basisdata als specifieke data op te halen
-                string sql = @"SELECT p.*, s.* FROM StadsOntwikkeling s 
-                       JOIN Project p ON s.Id = p.Id";
+//        public List<Stadsontwikkeling> GeefStadsontwikkelingen()
+//        {
+//            List<Stadsontwikkeling> lijst = new List<Stadsontwikkeling>();
+//            using (SqlConnection conn = new SqlConnection(_connectionString))
+//            {
+//                // SQL-query met JOIN om zowel basisdata als specifieke data op te halen
+//                string sql = @"SELECT p.*, s.* FROM StadsOntwikkeling s 
+//                       JOIN Project p ON s.Id = p.Id";
 
-                SqlCommand cmd = new SqlCommand(sql, conn);
-                conn.Open();
-                using (SqlDataReader reader = cmd.ExecuteReader())
-                {
-                    while (reader.Read())
-                    {
-                        var s = new Stadsontwikkeling();
-                        // Mapping van algemene velden
-                        s.Id = (int)reader["Id"];
-                        s.Titel = reader["Titel"].ToString();
+//                SqlCommand cmd = new SqlCommand(sql, conn);
+//                conn.Open();
+//                using (SqlDataReader reader = cmd.ExecuteReader())
+//                {
+//                    while (reader.Read())
+//                    {
+//                        var s = new Stadsontwikkeling();
+//                        // Mapping van algemene velden
+//                        s.Id = (int)reader["Id"];
+//                        s.Titel = reader["Titel"].ToString();
 
-                        // Mapping van specifieke velden 
-                        s.HeeftArchitecturaleWaarde = (bool)reader["HeeftArchitecturaleWaarde"];
-                        s.VergunningStatus = (VergunningStatus)reader["VergunningStatus"];
+//                        // Mapping van specifieke velden 
+//                        s.HeeftArchitecturaleWaarde = (bool)reader["HeeftArchitecturaleWaarde"];
+//                        s.VergunningStatus = (VergunningStatus)reader["VergunningStatus"];
 
-                        lijst.Add(s);
-                    }
-                }
-            }
-            return lijst;
-        }
-    }
-}
+//                        lijst.Add(s);
+//                    }
+//                }
+//            }
+//            return lijst;
+//        }
+//    }
+//}
